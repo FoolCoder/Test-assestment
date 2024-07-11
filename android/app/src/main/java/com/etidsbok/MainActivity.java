c654b2bf1d728f2a64491d25cc3195c28d40b3ca
mqazafi <muhammed.qazafi@volopa.com>
2024-07-11 14:57:53 +0500

    update app name

--- android/app/src/main/java/com/etidsbok/MainActivity.java
+++ android/app/src/main/java/com/etidsbok/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ETidsbok";
+    return "Test-Assesment";
   }
 
   /**
